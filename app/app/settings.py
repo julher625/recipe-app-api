5f06bf55316bc03448296c959dabbc55c27aa10b
Julian Hernandez <julian.hernndez@kinandcarta.com>
2022-05-01 23:57:56 -0500

    Added custom user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-idaw7ji69(#tucxv0pun%!f!bg=!clnh1a)c*hjduk+2!m_aj+'
+SECRET_KEY = 'django-insecure-idaw7ji69(#tucxv0pun%!f!bg=!' \
+             'clnh1a)c*hjduk+2!m_aj+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@domain.com'
+        password = 'password'
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test if the email for a new user is normalized"""
+        email = 'test@domaIn.com'
+        user = get_user_model().objects.create_user(email, 'password')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None,'password')
+
+    def test_create_new_superuser(self):
+        """Test create a new superuser"""
+
+        user = get_user_model().objects.create_superuser(
+            "test@domain.com",
+            'password'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
